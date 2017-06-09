9b2c812fabbcd4113dce09b8c0329a27c0581072
abelsan <abel@mit.edu>
2017-06-09 10:20:32 -0400

    language routing

--- server.js
+++ server.js
@@ -5,6 +5,25 @@ var app      	= express();
 app.use(express.static('start/Event'));
 app.use(express.static('start'));
 
+
+/* Serve page based on request domain */
+app.get('/', function(req, res){
+
+	var host = req.get('host');
+	console.log(host);
+
+	if (host === 'patroneshermosos.com'){
+		res.redirect('/es.html');
+	}
+	else if	(host === 'conpatron.com'){
+		res.redirect('/es.html');
+	}
+	else {
+		res.redirect('/en.html');
+	}
+});
+
+
 // start server
 app.listen(80, function(){
 	console.log('Listening on port 80');
